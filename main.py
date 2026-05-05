f2564580d5beb792b49fd1feb8500bc7da7d5837
Redwasp547 <spammajamma09@gmail.com>
2026-05-05 13:20:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,104 @@
-fjfhgfhgfdgs
+############################################################
+#################### IMPORT LIBRARIES ######################
+############################################################
+from time import sleep                     # <<< DO NOT MODIFY >>>
+sleep(5) # required for stability          # <<< DO NOT MODIFY >>>
+
+# Imports for MQTT communication           # <<< DO NOT MODIFY >>>
+import network                             # <<< DO NOT MODIFY >>>
+import json                                # <<< DO NOT MODIFY >>>
+from robust import MQTTClient        # <<< DO NOT MODIFY >>>
+
+from machine import ADC
+from math import log
+
+# Imports the library to make a random number. This is used to
+#    create a psuedo temperature value to transmit for demo
+#    purposes. You don't need this library for the project.
+import random
+
+############################################################
+################# SPECIFY PINS AND OBJECTS #################
+############################################################
+
+thermistor = ADC(26)
+
+############################################################
+##################### OTHER SETUP STUFF ####################
+############################################################
+
+V_in = 3.3 #[volts]
+R1 = 10000 #[ohms]
+
+#steinhart constants
+A = 1.129e-3
+B = 2.341e-4
+C = 8.767e-8
+
+# Wi-Fi and MQTT settings
+SSID = "WilfongEngr301" # Raspberry Pi 4 Wi-Fi name                               # <<< DO NOT MODIFY >>>
+PASSWORD = "BoilerUp" # Raspberry Pi 4 Wi-Fi password, WPA/WPA2 security          # <<< DO NOT MODIFY >>>
+MQTT_BROKER = "10.42.0.1"  # Raspberry Pi 4's IP                                  # <<< DO NOT MODIFY >>>
+TOPIC = "pico/data" # "pico/data" is just a label                                 # <<< DO NOT MODIFY >>>
+                    # It helps organize messages, like folders in a file system.  # <<< DO NOT MODIFY >>>
+                    # The TOPIC could be any string, but leave it as "pico/data"  # <<< DO NOT MODIFY >>>
+
+SENSOR_ID = "Team02"  # !!!-- CHANGE THIS AS DIRECTED BY DR. WILFONG --!!!
+
+# Connect to Wi-Fi                                          # <<< DO NOT MODIFY >>>
+wlan = network.WLAN(network.STA_IF)                         # <<< DO NOT MODIFY >>>
+wlan.active(True)                                           # <<< DO NOT MODIFY >>>
+wlan.config(pm = 0xa11140) # disable Wi-Fi low power mode   # <<< DO NOT MODIFY >>>
+wlan.connect(SSID, PASSWORD)                                # <<< DO NOT MODIFY >>>
+
+print("Attempting to connect to Wi-Fi")
+while not wlan.isconnected():                               # <<< DO NOT MODIFY >>>
+    pass                                                    # <<< DO NOT MODIFY >>>
+
+sleep(2)  # Extra delay for stability                       # <<< DO NOT MODIFY >>>
+print("Connected to Wi-Fi!")
+
+
+
+# Connect to MQTT broker with reconnect support         # <<< DO NOT MODIFY >>>
+client = MQTTClient(f"client_{SENSOR_ID}", MQTT_BROKER) # <<< DO NOT MODIFY >>>
+client.DEBUG = True                                     # <<< DO NOT MODIFY >>>
+
+# Try to connect to MQTT broker                         # <<< DO NOT MODIFY >>>
+try:                                                    # <<< DO NOT MODIFY >>>
+    client.connect()                                    # <<< DO NOT MODIFY >>>
+    print("Connected to MQTT broker!")
+except Exception as e:                                  # <<< DO NOT MODIFY >>>
+    print("Failed to connect to MQTT broker:", e)
+
+
+############################################################
+####################### INFINITE LOOP ######################
+############################################################
+while True: 
+    # !!!-- Psuedo temperature sensor reading between 50 and 55 --!!!
+    # !!!-- You must use this variable name: temperature_sensor_reading --!!!
+    # !!!-- Currently, the temperature reading is just a random number for demo purposes --!!!
+    adc_value = thermistor.read_u16() #0 to 65535
+    V_out = (V_in / 65535) * adc_value #[volts]
+    Rt = (V_out * R1) / (V_in - V_out) #[ohms] thermistor resistance
+    TempK = 1 / (A + (B * log(Rt)) + (C * pow(log(Rt), 3)))
+    TempC = TempK - 273.15 #[Celsius]
+
+    temperature_sensor_reading = TempC
+    
+    # Create and send MQTT payload                               # <<< DO NOT MODIFY >>>
+    message_data = {                                             # <<< DO NOT MODIFY >>>
+        "sensorID": SENSOR_ID,                                   # <<< DO NOT MODIFY >>>
+        "temperatureReading": temperature_sensor_reading         # <<< DO NOT MODIFY >>>
+    }                                                            # <<< DO NOT MODIFY >>>
+    message_json = json.dumps(message_data)  # Convert to JSON   # <<< DO NOT MODIFY >>>
+    
+    # Try to publish message to MQTT broker                                    # <<< DO NOT MODIFY >>>
+    try:                                                                       # <<< DO NOT MODIFY >>>
+        client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
+        print(f"Published: {message_json}") # Print MQTT payload to the Shell
+    except Exception as e:                                                     # <<< DO NOT MODIFY >>>
+        print("Publish failed:",e)
+    
+    sleep(2) # Send MQTT payload every 2 seconds
