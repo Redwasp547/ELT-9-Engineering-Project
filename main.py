073eab6aa656866b775ca566bb0e10b80d4eb8db
Redwasp547 <spammajamma09@gmail.com>
2026-05-08 09:45:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -48,11 +48,15 @@ R1 = 10000 #[ohms]
 
 start_flag = False
 
+userSelect = 1
+
 password = (1, 2, 3, 4)
 passwordLength = 4
 wait = False
 
 r = ()
+
+repeat = 0
 
 #steinhart constants
 A = 1.129e-3
@@ -168,7 +172,30 @@ while True:
     TempC = TempK - 273.15 #[Celsius]
     print(TempC)
 
+    
     temperature_sensor_reading = TempC
+    
+    if userSelect == 1:
+        display.fill(0) # clears display
+        display.text("-> display temp", 0, 10)
+        display.text("   change pass", 0, 20)# display text starting at x=0, y=10
+        display.show()
+    elif userSelect == 0:
+        display.fill(0) # clears display
+        display.text("   display temp", 0, 10)
+        display.text("-> change pass", 0, 20)# display text starting at x=0, y=10
+        display.show()
+    if wait == False  : 
+        if y_joystick > 60000 and userSelect != 0:
+            userSelect -= 1
+            wait = True
+        elif y_joystick < 3000 and userSelect != 1:
+            userSelect += 1
+            wait = True
+    elif wait:
+        if y_joystick < 38000 and y_joystick > 28000 and z_joystick == False:
+            wait = False
+    print(userSelect)
     
     # Create and send MQTT payload                               # <<< DO NOT MODIFY >>>
     message_data = {                                             # <<< DO NOT MODIFY >>>
@@ -178,10 +205,12 @@ while True:
     message_json = json.dumps(message_data)  # Convert to JSON   # <<< DO NOT MODIFY >>>
     
     # Try to publish message to MQTT broker                                    # <<< DO NOT MODIFY >>>
-    try:                                                                       # <<< DO NOT MODIFY >>>
-        client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
-        print(f"Published: {message_json}") # Print MQTT payload to the Shell
-    except Exception as e:                                                     # <<< DO NOT MODIFY >>>
-        print("Publish failed:",e)
-    
-    sleep(10) # Send MQTT payload every 10 seconds
+    if repeat == 100:
+        try:                                                                       # <<< DO NOT MODIFY >>>
+            client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
+            print(f"Published: {message_json}") # Print MQTT payload to the Shell
+        except Exception as e:                                                     # <<< DO NOT MODIFY >>>
+            print("Publish failed:",e)
+        repeat = 0
+    repeat += 1
+    sleep(.1) # Send MQTT payload every 10 seconds
