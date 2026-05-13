305f7a2a3e6ba9059bbbcb0a48197089608d28d3
Redwasp547 <spammajamma09@gmail.com>
2026-05-13 07:53:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ newPass = True
 
 repeat = 0
 
+timer = 0
+
 #steinhart constants
 A = 1.129e-3
 B = 2.341e-4
@@ -206,67 +208,123 @@ while True:
             screen = 0
             sleep(1)
     if userSelect == 0 and screen == 2:
+        r = ()
+        while start_flag == False:
+            display.fill(0) # clears display
+            display.text("Enter old", 0, 10)
+            display.text("password", 0, 20)
+            display.show()
+    
+            x_joystick = x_joystick_pin.read_u16()
+            y_joystick = y_joystick_pin.read_u16()
+    
+            if len(r) == passwordLength and r == password:
+                start_flag = True
+                display.fill(0) # clears display
+                display.text("Correct password", 0, 10) # display text starting at x=0, y=10
+                display.show()
+                sleep(1)
+            elif len(r) == passwordLength and r != password:
+                r = ()
+                print("incorrect")
+                display.fill(0) # clears display
+                display.text("Wrong password", 0, 10)
+                display.text("Please wait 15 secs", 0, 20)# display text starting at x=0, y=10
+                display.show()
+                sleep(15)
+    
+            elif wait == False:
+                if x_joystick > 60000:
+                    r += (1,)
+                    wait = True
+                elif y_joystick < 3000:
+                    r += (2,)
+                    wait = True
+                elif y_joystick >  60000:
+                    r += (3,)
+                    wait = True
+                elif x_joystick < 3000:
+                    r += (4,)
+                    wait = True
+                elif y_joystick > 60000 and x_joystick > 60000:
+                    r += (5,)
+                    wait = True
+                elif y_joystick > 60000 and x_joystick < 3000:
+                    r += (6,)
+                    wait = True
+                elif y_joystick < 3000 and x_joystick > 60000:
+                    r += (7,)
+                    wait = True
+                elif y_joystick < 3000 and x_joystick < 30000:
+                    r += (8,)
+                    wait = True
+            if wait:
+                if x_joystick < 38000 and y_joystick < 38000 and x_joystick > 28000 and y_joystick > 28000:
+                    wait = False
+            sleep(.1)
+
         password = ()
         display.fill(0) # clears display
         display.text("Enter new", 0, 10)
         display.text("password", 0, 20)
         display.show()
         
-        while password <= passwordLength:
+        while len(password) <= passwordLength:
             x_joystick = x_joystick_pin.read_u16()
             y_joystick = y_joystick_pin.read_u16()
             z_joystick = z_joystick_pin.is_pressed
-            print("in while loop"
-                  )
-            if x_joystick > 60000:
-                password += (1,)
-                wait = True
-                newPass = True
-                print("1")
-            elif y_joystick < 3000:
-                password += (2,)
-                wait = True
-                newPass = True
-                print("2")
-            elif y_joystick >  60000:
-                password += (3,)
-                wait = True
-                newPass = True
-                print("3")
-            elif x_joystick < 3000:
-                password += (4,)
-                wait = True
-                newPass = True
-                print("4")
-            elif y_joystick > 60000 and x_joystick > 60000:
-                password += (5,)
-                wait = True
-                newPass = True
-                print("5")
-            elif y_joystick > 60000 and x_joystick < 3000:
-                password += (6,)
-                wait = True
-                newPass = True
-                print("6")
-            elif y_joystick < 3000 and x_joystick > 60000:
-                password += (7,)
-                wait = True
-                newPass = True
-                print("7")
-            elif y_joystick < 3000 and x_joystick < 30000:
-                password += (8,)
-                wait = True
-                newPass = True
-                print("8")
-            if wait:
-                if x_joystick < 38000 and y_joystick < 38000 and x_joystick > 28000 and y_joystick > 28000:
-                    wait = False
+            print("in while loop")
+            if newPass == False:
+                if x_joystick > 60000:
+                    password += (1,)
+
+                    newPass = True
+                    print("1")
+                elif y_joystick < 3000:
+                    password += (2,)
+
+                    newPass = True
+                    print("2")
+                elif y_joystick >  60000:
+                    password += (3,)
+
+                    newPass = True
+                    print("3")
+                elif x_joystick < 3000:
+                    password += (4,)
+
+                    newPass = True
+                    print("4")
+                elif y_joystick > 60000 and x_joystick > 60000:
+                    password += (5,)
+
+                    newPass = True
+                    print("5")
+                elif y_joystick > 60000 and x_joystick < 3000:
+                    password += (6,)
+
+                    newPass = True
+                    print("6")
+                elif y_joystick < 3000 and x_joystick > 60000:
+                    password += (7,)
+
+                    newPass = True
+                    print("7")
+                elif y_joystick < 3000 and x_joystick < 30000:
+                    password += (8,)
+
+                    newPass = True
+                    print("8")
+            if newPass:
+                if x_joystick < 38000 and y_joystick < 38000 and x_joystick > 28000 and y_joystick > 28000:            
                     newPass = False
-            if wait == False and newPass == False:
-                newPass = False
             print(password)
             sleep(.1)
-                
+
+        display.fill(0) # clears display
+        display.text("password Changed", 0, 10)
+        display.show() 
+        sleep(1)
         screen = 0
 
 
@@ -299,4 +357,84 @@ while True:
             print("Publish failed:",e)
         repeat = 0
     repeat += 1
+
+    if timer >= 10000:
+        timer = 0
+        r = ()
+        while start_flag == False:
+            display.fill(0) # clears display
+            display.text("Enter Password", 0, 10) # display text starting at x=0, y=10
+            display.show()
+            adc_value = thermistor.read_u16()
+            x_joystick = x_joystick_pin.read_u16()
+            y_joystick = y_joystick_pin.read_u16()
+            V_out = (V_in / 65535) * adc_value #[volts]
+            Rt = (V_out * R1) / (V_in - V_out) #[ohms] thermistor resistance
+            TempK = 1 / (A + (B * log(Rt)) + (C * pow(log(Rt), 3)))
+            TempC = TempK - 273.15 #[Celsius]
+            print(TempC)
+    
+            if len(r) == passwordLength and r == password:
+                start_flag = True
+                display.fill(0) # clears display
+                display.text("Correct password", 0, 10) # display text starting at x=0, y=10
+                display.show()
+                sleep(1)
+            elif len(r) == passwordLength and r != password:
+                r = ()
+                print("incorrect")
+                display.fill(0) # clears display
+                display.text("Wrong password", 0, 10)
+                display.text("Please wait 15 secs", 0, 20)# display text starting at x=0, y=10
+                display.show()
+                sleep(15)
+    
+            elif wait == False:
+                if x_joystick > 60000:
+                    r += (1,)
+                    wait = True
+                elif y_joystick < 3000:
+                    r += (2,)
+                    wait = True
+                elif y_joystick >  60000:
+                    r += (3,)
+                    wait = True
+                elif x_joystick < 3000:
+                    r += (4,)
+                    wait = True
+                elif y_joystick > 60000 and x_joystick > 60000:
+                    r += (5,)
+                    wait = True
+                elif y_joystick > 60000 and x_joystick < 3000:
+                    r += (6,)
+                    wait = True
+                elif y_joystick < 3000 and x_joystick > 60000:
+                    r += (7,)
+                    wait = True
+                elif y_joystick < 3000 and x_joystick < 30000:
+                    r += (8,)
+                    wait = True
+            if wait:
+                if x_joystick < 38000 and y_joystick < 38000 and x_joystick > 28000 and y_joystick > 28000:
+                    wait = False
+
+            # Create and send MQTT payload                               # <<< DO NOT MODIFY >>>
+            message_data = {                                             # <<< DO NOT MODIFY >>>
+                "sensorID": SENSOR_ID,                                   # <<< DO NOT MODIFY >>>
+                "temperatureReading": temperature_sensor_reading         # <<< DO NOT MODIFY >>>
+            }                                                            # <<< DO NOT MODIFY >>>
+            message_json = json.dumps(message_data)  # Convert to JSON   # <<< DO NOT MODIFY >>>
+            
+            # Try to publish message to MQTT broker                                    # <<< DO NOT MODIFY >>>
+            if repeat == 100:
+                try:                                                                       # <<< DO NOT MODIFY >>>
+                    client.publish(TOPIC, message_json, retain=True) # Send MQTT payload   # <<< DO NOT MODIFY >>>
+                    print(f"Published: {message_json}") # Print MQTT payload to the Shell
+                except Exception as e:                                                     # <<< DO NOT MODIFY >>>
+                    print("Publish failed:",e)
+                repeat = 0
+            repeat += 1
+            sleep(.1)
+    timer += 1
+
     sleep(.1) # Send MQTT payload every 10 seconds
